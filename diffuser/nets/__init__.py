1614563de440cc23177a1875c1149fd921b813e4
zbzhu99 <zbzhu99@icloud.com>
2023-10-02 09:13:41 +0000

    Add 1d transformer, no attention mask for now

--- diffuser/nets/__init__.py
+++ diffuser/nets/__init__.py
@@ -1,6 +1,7 @@
 from .helpers import GaussianPolicy
 from .stational import Critic, DiffusionPolicy, InverseDynamic, Value
 from .temporal import DiffusionPlanner
+from .transformer import TransformerTemporalModel
 
 __all__ = [
     "DiffusionPolicy",
@@ -9,4 +10,5 @@ __all__ = [
     "GaussianPolicy",
     "DiffusionPlanner",
     "InverseDynamic",
+    "TransformerTemporalModel",
 ]
---
+++ tests/test_transformer.py
@@ -0,0 +1,26 @@
+import jax
+
+from diffuser.nets import TransformerTemporalModel
+
+
+def test_transformer_1d():
+    feat_in = 32
+    horizon = 10
+    x = jax.random.normal(jax.random.PRNGKey(0), (10, horizon, feat_in))
+
+    model = TransformerTemporalModel(
+        in_channels=feat_in,
+        n_heads=4,
+        d_head=32,
+        depth=2,
+    )
+    params = model.init(
+        jax.random.PRNGKey(0), x, None
+    )
+    output = model.apply(params, x, None)
+
+    print(output.shape)
+
+
+if __name__ == '__main__':
+    test_transformer_1d()
