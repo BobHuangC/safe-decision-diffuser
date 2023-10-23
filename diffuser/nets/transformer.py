2e5ad64e73702d6a3a25674ead8249caf614a954
BobHuangC <huang2003bo@sjtu.edu.cn>
2023-10-23 10:05:42 +0000

    fix embedding of transformer Implement the embedding of transformer similar to temporal.py

--- diffuser/nets/transformer.py
+++ diffuser/nets/transformer.py
@@ -11,13 +11,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import jax
+import distrax
 
 import flax.linen as nn
 import jax.numpy as jnp
 
 from diffuser.nets.attention import BasicTransformerBlock, StylizationBlock
 from diffuser.nets.helpers import TimeEmbedding
-
+from .helpers import mish
 
 class TransformerTemporalModel(nn.Module):
     r"""
@@ -85,19 +87,90 @@ class TransformerTemporalModel(nn.Module):
         if self.time_embed_dim is not None:
             self.stylization_block = StylizationBlock(self.in_channels, self.time_embed_dim, self.dropout)
 
-    def __call__(self, hidden_states, timesteps, context, deterministic=True):
+    def __call__(
+        self, 
+        hidden_states, 
+        timesteps, 
+        env_ts, 
+        returns_to_go,
+        cost_returns_to_go,
+        use_dropout, 
+        force_dropout,
+        context, 
+        deterministic=True):
         batch, length, channels = hidden_states.shape
         residual = hidden_states
         hidden_states = self.proj_in(self.norm(hidden_states))
         time_embed = self.time_encoder(timesteps)
+        
+        env_ts_emb = nn.Embed(self.max_traj_length, self.dim)(env_ts)
+        emb = jnp.stack([time_embed, env_ts_emb], axis=1)
+
+        act_fn = mish
+        mask_dist = None
+        if self.returns_condition:
+            returns_mlp = nn.Sequential(
+                [
+                    nn.Dense(self.dim),
+                    act_fn,
+                    nn.Dense(self.dim * 4),
+                    act_fn,
+                    nn.Dense(self.dim),
+                ]
+            )
+            mask_dist = distrax.Bernoulli(probs=1 - self.condition_dropout)
+
+        if self.cost_returns_condition:
+            assert self.returns_condition is True
+            cost_returns_mlp = nn.Sequential(
+                [
+                    nn.Dense(self.dim),
+                    act_fn,
+                    nn.Dense(self.dim * 4),
+                    act_fn,
+                    nn.Dense(self.dim),
+                ]
+            )
+
+        
+
+        if self.returns_condition:
+            assert returns_to_go is not None
+            returns_to_go = returns_to_go.reshape(-1, 1)
+            returns_embed = returns_mlp(returns_to_go)
+            if use_dropout:
+                rng, sample_key = jax.random.split(rng)
+                mask = mask_dist.sample(
+                    seed=sample_key, sample_shape=(returns_embed.shape[0], 1)
+                )
+                returns_embed = returns_embed * mask
+
+            if force_dropout:
+                returns_embed = returns_embed * 0
+            emb = jnp.concatenate([emb, jnp.expand_dims(returns_embed, 1)], axis=1)
+
+        if self.cost_returns_condition:
+            assert cost_returns_to_go is not None
+            cost_returns = cost_returns_to_go.reshape(-1, 1)
+            cost_returns_embed = cost_returns_mlp(cost_returns)
+            if use_dropout:
+                cost_returns_embed = cost_returns_embed * mask
+
+            if force_dropout:
+                cost_returns_embed = cost_returns_embed * 0
+            emb = jnp.concatenate([emb, jnp.expand_dims(cost_returns_embed, 1)], axis=1)
+
+        emb = nn.LayerNorm()(emb)
+        emb = emb.reshape(-1, emb.shape[1] * emb.shape[2])
+
 
         for transformer_block in self.transformer_blocks:
-            hidden_states = transformer_block(hidden_states, time_embed, context, deterministic=deterministic)
+            hidden_states = transformer_block(hidden_states, emb, context, deterministic=deterministic)
 
         hidden_states = self.proj_out(hidden_states)
 
         if self.time_embed_dim is not None:
-            hidden_states = self.stylization_block(hidden_states, time_embed) + residual
+            hidden_states = self.stylization_block(hidden_states, emb) + residual
         else:
             hidden_states = hidden_states + residual
         return self.dropout_layer(hidden_states, deterministic=deterministic)
